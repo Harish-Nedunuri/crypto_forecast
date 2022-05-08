ae15dd870fe85ac99cc605f5bdc117bd06e44ebd
Harish-Nedunuri <harish.nedunuri@outlook.com>
2022-05-08 13:43:52 +0100

    three tbles created

--- psql_crypto_schema.sql
+++ psql_crypto_schema.sql
@@ -16,3 +16,34 @@ CREATE TABLE "btc_prices"(
           volume_currency DOUBLE PRECISION,
           currency_code   VARCHAR (10)
 );
+
+--Schema for crypto_prices table
+DROP TABLE IF EXISTS "crypto_prices";
+CREATE TABLE "crypto_prices"(
+   time            TIMESTAMP WITH TIME ZONE NOT NULL,
+   opening_price   DOUBLE PRECISION,
+   highest_price   DOUBLE PRECISION,
+   lowest_price    DOUBLE PRECISION,
+   closing_price   DOUBLE PRECISION,
+   volume_crypto   DOUBLE PRECISION,
+   volume_btc      DOUBLE PRECISION,
+   currency_code   VARCHAR (10)
+);
+
+--Schema for eth_prices table
+DROP TABLE IF EXISTS "eth_prices";
+CREATE TABLE "eth_prices"(
+   time            TIMESTAMP WITH TIME ZONE NOT NULL,
+   opening_price   DOUBLE PRECISION,
+   highest_price   DOUBLE PRECISION,
+   lowest_price    DOUBLE PRECISION,
+   closing_price   DOUBLE PRECISION,
+   volume_eth      DOUBLE PRECISION,
+   volume_currency DOUBLE PRECISION,
+   currency_code   VARCHAR (10)
+);
+
+--Timescale specific statements to create hypertables for better performance
+SELECT create_hypertable('btc_prices', 'time');
+SELECT create_hypertable('eth_prices', 'time');
+SELECT create_hypertable('crypto_prices', 'time');
